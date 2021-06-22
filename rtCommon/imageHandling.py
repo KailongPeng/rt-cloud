329df98efb9fdf4c7170e4443be616ff05dbf76b
KailongPeng <kailongpeng001@gmail.com>
2021-06-22 06:53:41 -0400

    dcm2niix

--- rtCommon/imageHandling.py
+++ rtCommon/imageHandling.py
@@ -262,7 +262,8 @@ def saveAsNiftiImage(dicomDataObject, fullNiftiFilename, cfg, reference):
 def convertDicomFileToNifti(dicomFilename, niftiFilename):
     global binPath
     if binPath is None:
-        result = subprocess.run(['which', 'python'], stdout=subprocess.PIPE)
+        # result = subprocess.run(['which', 'python'], stdout=subprocess.PIPE)
+        result = subprocess.run(['which', 'dcm2niix'], stdout=subprocess.PIPE)
         binPath = result.stdout.decode('utf-8')
         binPath = os.path.dirname(binPath)
     dcm2niiCmd = os.path.join(binPath, 'dcm2niix')
@@ -272,7 +273,6 @@ def convertDicomFileToNifti(dicomFilename, niftiFilename):
     cmd = [dcm2niiCmd, '-s', 'y', '-b', 'n', '-o', outPath, '-f', outName,
            dicomFilename]
     subprocess.run(cmd, shell=False, stdout=subprocess.DEVNULL)
-
 
 def niftiToMem(niftiImg):
     '''
